0f70247ded56719b130b1be96d95bc041f4ec33c
jihwan <jihwan5580@gmail.com>
2026-04-23 20:29:45 +0900

    bookbutton

--- Assets/1_Scripts/1_ScriptableObject/ItemData.cs
+++ Assets/1_Scripts/1_ScriptableObject/ItemData.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "Item/ItemData", order = 1)]
 public class ItemData : ScriptableObject
 {
-    public string itemName;
-    public string itemDescription;
+    public string itemName;             // 아이템 이름
+    public string itemDescription;      // 아이템 설명
 
     public DateTime itemExpireDate;
     public DateTime itemCreateDate;
