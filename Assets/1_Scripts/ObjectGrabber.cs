737e08a8403b8f8c6ff756beaeef15e1bb5864e3
FACT0R1AL <factorial4861@gmail.com>
2026-04-23 19:29:34 +0900

    GameManager added

--- Assets/1_Scripts/ObjectGrabber.cs
+++ Assets/1_Scripts/ObjectGrabber.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class ObjectGrabber : MonoBehaviour
 {
@@ -8,69 +9,85 @@ public class ObjectGrabber : MonoBehaviour
     public float grabDistance = 5f;
     public float holdDistance = 2f;
     public float moveSpeed = 10f;
-
-    [Header("Rotate Settings")]
     public float rotateSpeed = 5f;
+    
+    [Header("About GrabbedObject")]
+    private GameObject grabbedObject;
+    private Rigidbody objectRb;
+    private bool isHolding = false;
+    private bool isBack = false;
 
-    Rigidbody grabbedObject;
-    bool isHolding = false;
+    private Vector3 lastConveyorPos;
+    private Quaternion lastConveyorRot;
 
-    FPSController fpsController;
+    // FPSController fpsController;
 
-    void Start()
-    {
-        fpsController = GetComponent<FPSController>();
-
-        if (fpsController == null)
-            fpsController = GetComponentInParent<FPSController>();
-    }
+    // void Start()
+    // {
+    //     fpsController = GetComponent<FPSController>();
+    //
+    //     if (fpsController == null)
+    //         fpsController = GetComponentInParent<FPSController>();
+    // }
 
     void Update()
     {
         // E 키로 잡기 / 놓기
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !isBack)
         {
             if (isHolding)
+            {
                 DropObject();
+            }
             else
+            {
                 TryGrab();
+            }
         }
 
-        if (isHolding && grabbedObject != null)
+        if (isHolding && objectRb is not null)
         {
             HoldObject();
             RotateObject();
         }
+        
+        Debug.DrawRay(cameraTransform.position, cameraTransform.forward * grabDistance, Color.red);
     }
 
     void TryGrab()
     {
-        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward * grabDistance);
         RaycastHit hit;
-
+        
         if (Physics.Raycast(ray, out hit, grabDistance))
         {
             if (hit.collider.CompareTag("Object"))
             {
-                Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                grabbedObject = hit.collider.gameObject;
+                objectRb = grabbedObject.GetComponent<Rigidbody>();
 
-                if (rb != null)
+                if (objectRb is null)
                 {
-                    grabbedObject = rb;
-                    grabbedObject.useGravity = false;
-                    grabbedObject.linearVelocity = Vector3.zero;
-                    grabbedObject.angularVelocity = Vector3.zero;
-
-                    isHolding = true;
-
-                    // 이동 + 시점 완전 차단
-                    if (fpsController != null)
-                    {
-                        fpsController.canLook = false;
-                        fpsController.canMove = false;
-                        fpsController.ResetView(); // 정면 바라보기
-                    }
+                    Debug.LogWarning("grabbedObject has no Rigidbody");
                 }
+                else
+                {
+                    objectRb.useGravity = false;
+                    objectRb.freezeRotation = true;
+                    isHolding = true;
+                    lastConveyorPos = grabbedObject.transform.position + new Vector3(0f, 0.5f, 0f);
+                    lastConveyorRot = grabbedObject.transform.rotation;
+                    
+                    Debug.Log(lastConveyorPos);
+                }
+                    
+                    // // 이동 + 시점 완전 차단
+                    // if (fpsController != null)
+                    // {
+                    //     fpsController.canLook = false;
+                    //     fpsController.canMove = false;
+                    //     fpsController.ResetView(); // 정면 바라보기
+                    // }
             }
         }
     }
@@ -78,9 +95,9 @@ public class ObjectGrabber : MonoBehaviour
     void HoldObject()
     {
         Vector3 targetPos = cameraTransform.position + cameraTransform.forward * holdDistance;
-
-        Vector3 direction = targetPos - grabbedObject.position;
-        grabbedObject.linearVelocity = direction * moveSpeed;
+        
+        Vector3 direction = targetPos - grabbedObject.transform.position;
+        objectRb.linearVelocity = direction * moveSpeed;
     }
 
     void RotateObject()
@@ -98,15 +115,49 @@ public class ObjectGrabber : MonoBehaviour
 
     void DropObject()
     {
-        grabbedObject.useGravity = true;
+        StartCoroutine(BackToConveyor());
+
+        // // 다시 이동 + 시점 허용
+        // if (fpsController != null)
+        // {
+        //     fpsController.canLook = true;
+        //     fpsController.canMove = true;
+        // }
+    }
+
+    IEnumerator BackToConveyor()
+    {
+        float t = 0f;
+
+        Vector3 startPos = grabbedObject.transform.position;
+        Vector3 endPos = lastConveyorPos;
+        
+        Quaternion startRot = grabbedObject.transform.rotation;
+        Quaternion endRot = lastConveyorRot;
+
+        isBack = true;
+        
+        while (t <= 1f)
+        {
+            t += Time.deltaTime;
+            
+            grabbedObject.transform.rotation = Quaternion.Lerp(startRot, endRot, t);
+            grabbedObject.transform.position = Vector3.Lerp(startPos, endPos, t);
+            
+            yield return null;
+        }
+        
+        grabbedObject.transform.position = endPos;
+        grabbedObject.transform.rotation = endRot;
+        
+        objectRb.useGravity = true;
+        objectRb.freezeRotation = false;
+        objectRb.linearVelocity = Vector3.zero;
+        objectRb = null;
         grabbedObject = null;
         isHolding = false;
+        isBack = false;
 
-        // 다시 이동 + 시점 허용
-        if (fpsController != null)
-        {
-            fpsController.canLook = true;
-            fpsController.canMove = true;
-        }
+        yield return null;
     }
 }
